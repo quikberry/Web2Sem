3d2d82bd39cceeb524cff56ba82e8c4d6253f3e5
Sergey Movchan <quikberry@list.ru>
2025-06-10 09:14:12 +0300

    Три виджета. Тестовые

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.movie_list, name='movie_list'),
-    path('movie/<int:pk>/', views.movie_detail, name='movie_detail'),
+    path('', views.index, name='index'),
+    path('movies/', views.movie_list, name='movie_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie
+from django.shortcuts import render
+from django.db.models import Count, Avg
+from .models import Movie, Session, Review
 
 def movie_list(request):
     movies = Movie.objects.all()
@@ -8,3 +10,24 @@ def movie_list(request):
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     return render(request, 'core/movie_detail.html', {'movie': movie})
+
+def index(request):
+        #Ближайшие релизы
+    upcoming_movies = Movie.objects.filter().order_by('created_at')[:5]
+
+    #Частопосещаемые
+    popular_by_sessions = Movie.objects.annotate(
+        session_count=Count('session')
+    ).order_by('-session_count')[:5]
+
+    #Лучшие по оценкам
+    top_rated = Movie.objects.annotate(
+        avg_rating=Avg('review__rating')
+    ).filter(avg_rating__isnull=False).order_by('-avg_rating')[:5]
+
+    context = {
+        'upcoming_movies': upcoming_movies,
+        'popular_by_sessions': popular_by_sessions,
+        'top_rated': top_rated,
+    }
+    return render(request, 'core/index.html', context)
